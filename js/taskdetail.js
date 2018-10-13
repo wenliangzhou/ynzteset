685e5765931d09460b3d8763db78fdf833732926
wenliang <752758299@qq.com>
2018-10-13 15:04:44 +0800

    任务详情100%……

--- js/taskdetail.js
+++ js/taskdetail.js
@@ -2,4 +2,11 @@
     $('.tishi').click(function () {
         sessionStorage.setItem('from','pageA');
     });
+    $(".lunbo").slick({
+        vertical: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow:'<span class="prev"><img src="img/dingzhi/top.png" alt=""></span>',
+        nextArrow:'<span class="next"><img src="img/dingzhi/bot.png" alt=""></span>'
+    });
 }())
